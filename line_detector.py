e164610e7bb405cd57c4a6868553c1334a23bda6
nlin004 <nilnahtan@gmail.com>
2022-01-31 17:29:19 -0800

    Line detection improved. Still needs work at 0 degrees.

--- line_detector.py
+++ line_detector.py
@@ -8,7 +8,7 @@ import json
 source = 1 #0 is native, 1 is external webcam
 camera = cv.VideoCapture(source, cv.CAP_DSHOW)
 camera.set(cv.CAP_PROP_EXPOSURE, -7) # change to -1 for internal camera, -7 for FISHEYE, -4 for Microsoft hd3000
-MIN_AREA = 1000
+MIN_AREA = 500
 
 def sobel_edge(frame):
     depth = cv.CV_16S
@@ -29,19 +29,35 @@ def laplace_edge(frame):
     kernel_size = 3
     src = cv.GaussianBlur(frame, (3,3), 0)
     src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    # dst = cv.adaptiveThreshold(src_gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,11,2)
+    # dst  = cv.Canny(src_gray, 50, 150, apertureSize=3)
     dst = cv.Laplacian(src_gray, ddepth, ksize=kernel_size)
     abs_dst = cv.convertScaleAbs(dst)
     return abs_dst
 
 def isRect(cnt, approx, ar):
-    return len(approx) == 4 and cv.contourArea(cnt) > MIN_AREA and not (0.8 <= ar <= 1.2)
+    return len(approx) == 4 and cv.contourArea(cnt) > MIN_AREA and not (0.8 <= ar <= 1.1)
 
 def maskColor(frame):
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    mask = cv.inRange(frame, (0,0,0), (50,50,50))
-    frame = cv.bitwise_and(frame, frame, mask = mask)
-    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    return frame 
+    # frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    # lower_gray = np.array([0, 0, 0], np.uint8)
+    # upper_gray = np.array([40, 58, 48], np.uint8)
+    # # mask = cv.InRangeS(imgHSV, cv.Scalar(0, 0, 0, 0), cv.Scalar(180, 255, 30, 0), imgThreshold)
+    # # thresh = cv.threshold(frame, 60, 255, cv.THRESH_BINARY_INV)
+    # mask_gray = cv.inRange(frame, lower_gray, upper_gray)
+    # # img_res = cv.bitwise_and(mask_gray, mask_gray, mask = mask_gray)
+
+    # # mask = cv.inRange(frame, (0,0,0), (90,129,150))
+    # frame = cv.bitwise_and(frame, frame, mask = mask_gray)
+    # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # return frame # frame 
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    blurred = cv.GaussianBlur(gray, (5,5), 3)
+    ret, thresh = cv.threshold(blurred, 45, 255, cv.THRESH_BINARY_INV)
+    thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, (13,13))
+    # thresh = cv.cvtColor(thresh, cv.COLOR_GRAY2BGR)
+    return thresh
 
 def findRect(frame, output):
     contours, _ = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -61,18 +77,27 @@ def findRect(frame, output):
             p1 = (cols-1, righty)
             p2 = (0, lefty)
 
+            
             cv.line(output, p1,p2,(0,0,255),2)
             print(get_angle(p1, p2))
             cv.putText(output, f"{str(get_angle(p1,p2))} degrees", (int(x), int(y-10)), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255)) 
 
 def get_angle(p1,p2):
+    
     m = ((p1[1] - p2[1]) / (p1[0] - p2[0]))
-    return format(math.atan(m) * -180 / math.pi, '.2f')
+    return format(math.atan(m) * -180 / math.pi, '.0f')
          
 
 def detect_line(frame):
     masked_frame = maskColor(frame)
     findRect(masked_frame, frame)
+
+    cv.imshow("Line Mask", masked_frame)
+
+    if cv.waitKey(1) & 0xFF is ord('q'):
+        return frame
+
+
     return frame
 
 
